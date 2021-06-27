29c1bd1368297e7991a0a48c8c92235f0dcc825a
Tomás Malvissi <tomasmalvissi@hotmail.com>
2021-06-26 23:30:37 -0300

    include proyect

---
+++ Preguntero/Preguntero/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Preguntero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSemanas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPreguntas = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpFechaIni = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(377, 85);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese la cantidad de semanas:";
+            // 
+            // txtSemanas
+            // 
+            this.txtSemanas.Location = new System.Drawing.Point(228, 60);
+            this.txtSemanas.Name = "txtSemanas";
+            this.txtSemanas.Size = new System.Drawing.Size(100, 20);
+            this.txtSemanas.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(166, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PREGUNTERO";
+            // 
+            // txtPreguntas
+            // 
+            this.txtPreguntas.Location = new System.Drawing.Point(47, 161);
+            this.txtPreguntas.Name = "txtPreguntas";
+            this.txtPreguntas.Size = new System.Drawing.Size(405, 145);
+            this.txtPreguntas.TabIndex = 4;
+            this.txtPreguntas.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ingrese la fecha de comienzo:";
+            // 
+            // dtpFechaIni
+            // 
+            this.dtpFechaIni.Location = new System.Drawing.Point(228, 106);
+            this.dtpFechaIni.Name = "dtpFechaIni";
+            this.dtpFechaIni.Size = new System.Drawing.Size(100, 20);
+            this.dtpFechaIni.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 358);
+            this.Controls.Add(this.dtpFechaIni);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPreguntas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSemanas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSemanas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtPreguntas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpFechaIni;
+    }
+}
+
---
+++ Preguntero/Preguntero/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preguntero
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //TODO: ver xq manda la misma hora salvo cuando debuggeo
+        //TODO: validar sabados y domingos de nuevo cuando lo asigno
+        //TODO: escribir error en elseif
+        public void RetornarPreguntas(int semana, DateTime fecha ,string[] pregunta) 
+        {
+            if (fecha.Day != 0 || fecha.Day != 6)
+            {
+                if (fecha >= DateTime.UtcNow)
+                {
+                    for (int i = 0; i < semana; i++)
+                    {
+                        Random rdn = new Random();
+                        int horas = rdn.Next(8, 18);    //creo un randon de la hora laboral
+                        int minutos = rdn.Next(60);
+                        TimeSpan ts = new TimeSpan(horas, minutos, 0);         //asigno la hora a la fecha que pickeo           
+                        fecha = fecha.Date + ts;
+                        txtPreguntas.Text += fecha.ToString("dddd, dd MMMM yyyy HH:mm:ss")+ " - " + pregunta[i] + "\n";
+                        fecha = fecha.AddDays(1);       //aumento la fecha
+                    }
+                }
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            int semanas = int.Parse(txtSemanas.Text);
+            DateTime fechaini = dtpFechaIni.Value;
+            string[] preguntas = { "Pregunta1", "Pregunta2", "Pregunta3", "Pregunta4", "Pregunta5", "Pregunta6", "Pregunta7" };
+            RetornarPreguntas(semanas, fechaini, preguntas);
+        }
+    }
+}
